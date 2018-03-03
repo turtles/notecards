0a24df9de87a44e821a6783f54c3067fc7dc1898
Leah Sandacz <sandas@outlook.com>
2018-03-03 02:39:22 -0800

    Fix compiler warnings

--- src/Components/NotecardEditor/NotecardList.js
+++ src/Components/NotecardEditor/NotecardList.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import {
   Row,Col,Input,
-  Button,
-  Nav, NavItem, NavLink
+  Button
 } from 'reactstrap'
 import NotecardEditorMenu from './NotecardEditorMenu.js'
 import '../../Styles/NotecardEditor.css'
@@ -21,7 +20,7 @@ const NotecardList = ({notecards,
       onExportNotecards={onExportNotecards} />
 
     {notecards.map((notecard, index) => (
-        <Row key={notecard.id} className={index%2==0 ? "notecardRow" : "altNotecardRow"}>
+        <Row key={notecard.id} className={index%2===0 ? "notecardRow" : "altNotecardRow"}>
           <Col xs="3">
             <Input value={notecard.word}
                 onChange={(e)=>onUpdateNotecard(notecard.id, e.target.value, notecard.definition)} />
